8417fb27552086a2c351f4f293e9150b3f91da23
Thinh Nguyen <thinh.nguyen@stunited.vn>
2022-08-18 18:10:40 +0700

    add

--- src/assets/dummy.js
+++ src/assets/dummy.js
@@ -46,7 +46,7 @@ export const championsData = [
         img: cardAshe,
         name: 'Lam Chiu',
         bg: bgAshe,
-        nickName: 'THE FROST ARCHER',
+        nickName: 'TRAP GIRL SP',
         role: 'SLOYAL',
         difficulty: 'MODERATE',
         description: `Iceborn warmother of the Avarosan tribe, Ashe commands the most populous horde in the north. Stoic, intelligent, and idealistic, yet uncomfortable with her role as leader, she taps into the ancestral magics of her lineage to wield a bow of True Ice. With her people's belief that she is the mythological hero Avarosa reincarnated, Ashe hopes to unify the Freljord once more by retaking their ancient, tribal lands.`,
@@ -57,12 +57,23 @@ export const championsData = [
         img: cardGaren,
         name: 'Tien Thinh',
         bg: bgGaren,
-        nickName: 'THE MIGHT OF DEMACIA',
+        nickName: 'WEREWOLF',
         role: 'FANINTELLIGENCE',
         difficulty: 'MODERATE',
-        description: `A proud and noble warrior, Garen fights as one of the Dauntless Vanguard. He is popular among his fellows, and respected well enough by his enemies—not least as a scion of the prestigious Crownguard family, entrusted with defending Demacia and its ideals. Clad in magic-resistant armor and bearing a mighty broadsword, Garen stands ready to confront mages and sorcerers on the field of battle, in a veritable whirlwind of righteous steel.`,
+        description: `See ya 12 a.m señorita, Non ho molto tempo adesso, Mamma Mia.`,
         video: 'Q7oyW6LjxrA',
         bgLarge: bgLargeGaren
+    },
+    {
+        img: cardLux,
+        name: 'Cong Nhat',
+        bg: bgLux,
+        nickName: 'THE LADY OF LUMINOSITY',
+        role: 'SLOYAL',
+        difficulty: 'MODERATE',
+        description: `Luxanna Crownguard hails from Demacia, an insular realm where magical abilities are viewed with fear and suspicion. Able to bend light to her will, she grew up dreading discovery and exile, and was forced to keep her power secret, in order to preserve her family's noble status. Nonetheless, Lux's optimism and resilience have led her to embrace her unique talents, and she now covertly wields them in service of her homeland.`,
+        video: 'khEBkz6A0oQ',
+        bgLarge: bgLargeLux
     },
     {
         img: cardAhri,
@@ -74,16 +85,5 @@ export const championsData = [
         description: 'Innately connected to the latent power of Runeterra, Ahri is a vastaya who can reshape magic into orbs of raw energy. She revels in toying with her prey by manipulating their emotions before devouring their life essence. Despite her predatory nature, Ahri retains a sense of empathy as she receives flashes of memory from each soul she consumes.',
         video: '0z7kfuJwGvs',
         bgLarge: bgLargeAhri
-    },
-    {
-        img: cardLux,
-        name: 'Lux',
-        bg: bgLux,
-        nickName: 'THE LADY OF LUMINOSITY',
-        role: 'MAGE',
-        difficulty: 'MODERATE',
-        description: `Luxanna Crownguard hails from Demacia, an insular realm where magical abilities are viewed with fear and suspicion. Able to bend light to her will, she grew up dreading discovery and exile, and was forced to keep her power secret, in order to preserve her family's noble status. Nonetheless, Lux's optimism and resilience have led her to embrace her unique talents, and she now covertly wields them in service of her homeland.`,
-        video: 'khEBkz6A0oQ',
-        bgLarge: bgLargeLux
     },
 ]
